6b3a1e7051c83374f8624a300c1ef68a91c19ae3
AlexDumitru17 <a.dumitru@student.tudelft.nl>
2023-04-04 23:28:33 +0200

    Added training for all ranklib algorithms

--- main.py
+++ main.py
@@ -1,16 +1,51 @@
-import tensorflow as tf
-import tensorflow_datasets as tfds
-
-def load_and_save_data():
-    # 10k
-    print("started")
-    ds = tfds.load("mslr_web")
+import subprocess
 
 
-def main():
-    load_and_save_data()
-    print("Hello World!")
+def train_ranklib_model(training_data, model_output, model_type, metric, validation_data):
+    ranklib_jar = "RankLib-2.18.jar"
+    command = f"java -jar {ranklib_jar} -train {training_data} -save {model_output} -ranker {model_type} -metric2t {metric} -validate {validation_data}"
+    print(f"Executing command: {command}")
 
+    with subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True) as process:
+        for line in process.stdout:
+            print(line.strip())
+
+    return_code = process.wait()
+    if return_code != 0:
+        print(f"RankLib process exited with code {return_code}")
+
+
+def test_ranklib_model(model_to_load, test_data, metric):
+    ranklib_jar = "RankLib-2.18.jar"
+    command = f"java -jar {ranklib_jar} -load {model_to_load}  -test {test_data} -metric2T {metric}"
+    print(f"Executing command: {command}")
+
+    with subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True) as process:
+        for line in process.stdout:
+            print(line.strip())
+
+    return_code = process.wait()
+    if return_code != 0:
+        print(f"RankLib process exited with code {return_code}")
 
 if __name__ == "__main__":
-    main()
+    metrics = ["NDCG@10", "MAP", "P@10"]
+    # Number of algorithms
+    n = 8
+    # TODO: Change fold nr
+    fold_nr = 2
+    for metric in metrics:
+        for model_nr in range(0, n):
+            training_data = f"data/mslr_web/Fold{fold_nr}/train.txt"
+            vali_data = f"data/mslr_web/Fold{fold_nr}/vali.txt"
+            model_output = f"models/Model{model_nr}Metric{metric}"
+            train_ranklib_model(training_data, model_output, model_nr, metric, vali_data)
+    # training_data = "data/mslr_web/Fold1/train.txt"
+    # test_data = "data/mslr_web/Fold1/test.txt"
+    # vali_data = "data/mslr_web/Fold1/vali.txt"
+    # model_output = "models/testModel.txt"
+    #
+    # model_type = 3  # For example, use RankNet
+    # metric = "NDCG@10"
+
+    # train_ranklib_model(training_data, model_output, model_type, metric, vali_data)
