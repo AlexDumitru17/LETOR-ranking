0ad249d633e50588d217eefc108c3874ba616e4a
AlexDumitru17 <a.dumitru@student.tudelft.nl>
2023-04-05 14:25:18 +0200

    Fixed bugs

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import subprocess
+import concurrent.futures
+from functools import partial
 
-
-def train_ranklib_model(training_data, model_output, model_type, metric, validation_data):
+ranker_configs = {
+    1: {'epoch': 50, 'lr': 0.001, 'node': 5},  # RankNet
+    4: {'r': 3, 'i': 20},  # Coordinate Ascent
+    5: {'epoch': 50, 'lr': 0.001, 'node': 5}, # LambdaRank
+    # 6: {'tree': 300, 'leaf': 10},  # LambdaMART
+    # Other rankers can use default parameters
+}
+def train_ranklib_model(training_data, model_output, model_type, metric, validation_data, ranker_config):
     ranklib_jar = "RankLib-2.18.jar"
-    command = f"java -jar {ranklib_jar} -train {training_data} -save {model_output} -ranker {model_type} -metric2t {metric} -validate {validation_data}"
+    ranker_params = " ".join([f"-{key} {value}" for key, value in ranker_config.items()])
+    command = f"java -jar {ranklib_jar} -train {training_data} -save {model_output} -ranker {model_type} {ranker_params} -metric2t {metric} -validate {validation_data}"
     print(f"Executing command: {command}")
 
     with subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True) as process:
@@ -20,9 +29,9 @@ def test_ranklib_model(model_to_load, test_data, metric):
     command = f"java -jar {ranklib_jar} -load {model_to_load}  -test {test_data} -metric2T {metric}"
     print(f"Executing command: {command}")
 
-    with subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True) as process:
-        for line in process.stdout:
-            print(line.strip())
+    # with subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True) as process:
+    #     for line in process.stdout:
+    #         print(line.strip())
 
     return_code = process.wait()
     if return_code != 0:
@@ -33,13 +42,15 @@ if __name__ == "__main__":
     # Number of algorithms
     n = 8
     # TODO: Change fold nr
-    fold_nr = 2
-    for metric in metrics:
-        for model_nr in range(0, n):
-            training_data = f"data/mslr_web/Fold{fold_nr}/train.txt"
-            vali_data = f"data/mslr_web/Fold{fold_nr}/vali.txt"
-            model_output = f"models/Model{model_nr}Metric{metric}"
-            train_ranklib_model(training_data, model_output, model_nr, metric, vali_data)
+    fold_nr = 3
+    with concurrent.futures.ProcessPoolExecutor(max_workers=3) as executor:
+        for metric in metrics:
+            for model_nr in range(0, n):
+                training_data = f"data/mslr_web/Fold{fold_nr}/train.txt"
+                vali_data = f"data/mslr_web/Fold{fold_nr}/vali.txt"
+                model_output = f"models/Model{model_nr}Metric{metric}Fold{fold_nr}"
+                ranker_config = ranker_configs.get(model_nr, {})
+                executor.submit(train_ranklib_model, training_data, model_output, model_nr, metric, vali_data, ranker_config)
     # training_data = "data/mslr_web/Fold1/train.txt"
     # test_data = "data/mslr_web/Fold1/test.txt"
     # vali_data = "data/mslr_web/Fold1/vali.txt"
